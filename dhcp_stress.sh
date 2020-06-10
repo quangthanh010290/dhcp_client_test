66ad60e68f505e59883d123cecf61750acfabe85
quangthanh010290 <thanh.le@veriksystems.com>
2020-06-10 11:36:01 +0700

    mac format error

--- dhcp_stress.sh
+++ dhcp_stress.sh
@@ -7,12 +7,12 @@ COL='\033[0;36m'
 NC='\033[0m' # No Color
 HERE="`pwd`"
 IF="eth1"
-MAC="00:00:00:00:82:bf"
 while true 
 do
 	hexchars="0123456789ABCDEF"
 	end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/-\1/g' )
 	MAC="00-60-2F$end"
+	MAC=${MAC//-/:}
 	echo -e "${GREEN}$MAC${NC}"
 	sudo ./dhcp_client_test -m ${MAC} -i ${IF}
 	sleep 3
