5b36f998b41ef815a52f87cce3922ac0ad975f62
quangthanh010290 <thanh.le@veriksystems.com>
2020-06-10 09:30:33 +0700

    update stress test script

--- dhcp_stress.sh
+++ dhcp_stress.sh
@@ -6,6 +6,6 @@ do
 	end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/-\1/g' )
 	MAC="00-60-2F$end"
 	echo -e "${GREEN}$MAC${NC}"
-	sudo dhcp_client_test -m ${MAC} -i ${IF}
+	sudo ./dhcp_client_test -m ${MAC} -i ${IF}
 	sleep 3
 done
