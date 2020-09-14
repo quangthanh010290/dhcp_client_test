12f8c787a04695a68bfa7094997d4a5b0b030d57
quangthanh010290 <thanh.le@veriksystems.com>
2020-09-14 14:22:59 +0700

    update stress test

--- dhcp_stress.sh
+++ dhcp_stress.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YEL='\033[0;33m'
@@ -6,14 +7,15 @@ PUR='\033[0;35m'
 COL='\033[0;36m'
 NC='\033[0m' # No Color
 HERE="`pwd`"
-# Change IF to the real interface on your PC
 IF="eth1"
+MAC="00:00:00:00:82:bf"
 while true
 do
 	hexchars="0123456789ABCDEF"
 	end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
 	MAC="00:60:2F$end"
 	echo -e "${GREEN}$MAC${NC}"
-	sudo dhcp_client_test -m ${MAC} -i ${IF}
-	sleep 3
+	client="client${end}"
+	sudo dhcp_client_test -m ${MAC} -i ${IF} --option12-hostname ${client}
+	sleep 0.3
 done
